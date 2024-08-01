970e7b1372fcaff11dd86ce40b0bd5ba45bb0c59
Hanif Budiyanto <hanifdiyer@gmail.com>
2024-08-01 02:55:55 +0200

    refactor, drop eigen completely

--- example/2pt_ransac.cpp
+++ example/2pt_ransac.cpp
@@ -5,11 +5,32 @@
 #include <opencv2/opencv.hpp>
 #include <random>
 #include <string>
+#include <tuple>
 
-void drawEpipolarLines(const cv::Mat &img1, const cv::Mat &img2,
-                       const cv::Mat &F,
-                       const std::vector<cv::Point2f> &points1,
-                       const std::vector<cv::Point2f> &points2) {
+using pointsVec = std::vector<cv::Point2f>;
+
+std::tuple<int, int> randomizeInts(int min_int, int max_int) {
+  // Generate two random integers
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<int> dis2(min_int, max_int);
+
+  std::set<int> numbers;
+  while (numbers.size() < 2) {
+    int rand_num = dis2(gen);
+    numbers.insert(rand_num);
+  }
+
+  auto it = numbers.begin();
+  int num1 = *it;
+  ++it;
+  int num2 = *it;
+
+  return std::make_tuple(num1, num2);
+}
+
+void drawEpilines(const cv::Mat &img1, const cv::Mat &img2, const cv::Mat &F,
+                  const pointsVec &points1, const pointsVec &points2) {
 
   cv::Mat img1_copy, img2_copy;
   img1.copyTo(img1_copy);
@@ -55,14 +76,66 @@ void drawEpipolarLines(const cv::Mat &img1, const cv::Mat &img2,
   cv::waitKey(0);
 }
 
-std::string _to2Sig(float number) {
-  // reduce float to 2 significant digits, return as a string
-  if (number == 0.0)
-    return "0.0"; // Handle zero separately
+std::tuple<pointsVec, pointsVec> matchFeatures(const cv::Mat &img1,
+                                               const cv::Mat &img2,
+                                               float ratio_thresh,
+                                               int num_top_matches) {
+  // Initialize the ORB detector
+  cv::Ptr<cv::ORB> orb = cv::ORB::create();
 
-  std::stringstream ss;
-  ss << std::scientific << std::setprecision(2) << number;
-  return ss.str();
+  // Convert images to grayscale if they are not already
+  cv::Mat img1Gray, img2Gray;
+  if (img1.channels() > 1) {
+    cv::cvtColor(img1, img1Gray, cv::COLOR_BGR2GRAY);
+  } else {
+    img1Gray = img1;
+  }
+  if (img2.channels() > 1) {
+    cv::cvtColor(img2, img2Gray, cv::COLOR_BGR2GRAY);
+  } else {
+    img2Gray = img2;
+  }
+
+  // Detect keypoints and compute descriptors
+  std::vector<cv::KeyPoint> keypoints1, keypoints2;
+  cv::Mat descriptors1, descriptors2;
+  orb->detectAndCompute(img1Gray, cv::noArray(), keypoints1, descriptors1);
+  orb->detectAndCompute(img2Gray, cv::noArray(), keypoints2, descriptors2);
+
+  // Use BFMatcher to match descriptors
+  cv::BFMatcher bfMatcher(cv::NORM_HAMMING);
+  std::vector<std::vector<cv::DMatch>> knnMatches;
+  bfMatcher.knnMatch(descriptors1, descriptors2, knnMatches, 2);
+
+  // Apply ratio test
+  std::vector<cv::DMatch> goodMatches;
+  for (const auto &match : knnMatches) {
+    if (match[0].distance < ratio_thresh * match[1].distance) {
+      goodMatches.push_back(match[0]);
+    }
+  }
+
+  // Sort matches by distance
+  std::sort(goodMatches.begin(), goodMatches.end());
+
+  // Limit the number of matches to the top N matches
+  if (goodMatches.size() > num_top_matches) {
+    goodMatches.resize(num_top_matches);
+  }
+
+  // Accumulating matched features into points1 and points2
+  pointsVec points1, points2;
+  int matchesSize = goodMatches.size();
+  points1.reserve(matchesSize);
+  points2.reserve(matchesSize);
+  for (const auto &match : goodMatches) {
+    cv::Point2f pt1 = keypoints1[match.queryIdx].pt;
+    cv::Point2f pt2 = keypoints2[match.trainIdx].pt;
+    points1.push_back(pt1);
+    points2.push_back(pt2);
+  }
+
+  return std::make_tuple(points1, points2);
 }
 
 int main() {
@@ -78,102 +151,34 @@ int main() {
     return -1;
   }
 
-  cv::Mat img1Gray, img2Gray;
-  cv::cvtColor(img1, img1Gray, cv::COLOR_BGR2GRAY);
-  cv::cvtColor(img2, img2Gray, cv::COLOR_BGR2GRAY);
-
-  // Initialize the ORB detector
-  cv::Ptr<cv::ORB> orb = cv::ORB::create();
-
-  // Detect keypoints and compute descriptors
-  std::vector<cv::KeyPoint> keypoints1, keypoints2;
-  cv::Mat descriptors1, descriptors2;
-  orb->detectAndCompute(img1Gray, cv::noArray(), keypoints1, descriptors1);
-  orb->detectAndCompute(img2Gray, cv::noArray(), keypoints2, descriptors2);
-
-  // Use BFMatcher to match descriptors
-  cv::BFMatcher bfMatcher(cv::NORM_HAMMING);
-  std::vector<std::vector<cv::DMatch>> knnMatches;
-  bfMatcher.knnMatch(descriptors1, descriptors2, knnMatches, 2);
-
-  // Apply ratio test
-  const float ratio_thresh = 0.85;
-  std::vector<cv::DMatch> goodMatches;
-  for (size_t i = 0; i < knnMatches.size(); i++) {
-    if (knnMatches[i][0].distance < ratio_thresh * knnMatches[i][1].distance) {
-      goodMatches.push_back(knnMatches[i][0]);
-    }
-  }
-
-  // Sort matches by distance
-  std::sort(goodMatches.begin(), goodMatches.end());
-
-  // Limit the number of matches to the top N matches
-  const int numTopMatches = 50;
-  if (goodMatches.size() > numTopMatches) {
-    goodMatches.resize(numTopMatches);
-  }
-
-  // Accumulating matched features into points1 and points2
-  std::vector<cv::Point2f> points1, points2;
-  int matchesSize = goodMatches.size();
-  points1.reserve(matchesSize), points2.reserve(matchesSize);
-  for (size_t i = 0; i < goodMatches.size(); i++) {
-    cv::Point2f pt1 = keypoints1[goodMatches[i].queryIdx].pt;
-    cv::Point2f pt2 = keypoints2[goodMatches[i].trainIdx].pt;
-    points1.push_back(pt1);
-    points2.push_back(pt2);
-  }
+  // Feature matching
+  float ratio_thresh = 0.85;
+  int num_top_matches = 100;
+  pointsVec points1, points2;
+  std::tie(points1, points2) =
+      matchFeatures(img1, img2, ratio_thresh, num_top_matches);
 
   // Generate two random integers
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_int_distribution<int> dis2(0, points1.size() - 1);
-
-  std::set<int> numbers;
-  while (numbers.size() < 2) {
-    int rand_num = dis2(gen);
-    numbers.insert(rand_num);
-  }
-
-  auto it = numbers.begin();
   int num1, num2;
-  num1 = *it;
-  ++it;
-  num2 = *it;
+  std::tie(num1, num2) = randomizeInts(0, points1.size() - 1);
 
   // Push back two random visual corespondences
-  std::vector<cv::Point2f> points1_est = {points1[num1], points1[num2]};
-  std::vector<cv::Point2f> points2_est = {points2[num1], points2[num2]};
+  pointsVec points1_est = {points1[num1], points1[num2]};
+  pointsVec points2_est = {points2[num1], points2[num2]};
 
-  // Find the essential matrix
   cv::Mat k = (cv::Mat_<double>(3, 3) << 1280.7, 0.0, 969.4257, 0.0, 1281.2,
                639.7227, 0.0, 0.0, 1.0);
-  std::vector<cv::Mat> E = PM::findEssential(points1_est, points2_est, k);
 
-  /*
-  cv::Mat F = k.t().inv() * E[0] * k.inv();
-  cv::Mat F_normalized = PM::_normalizeF(F);
-
-  drawEpipolarLines(img1, img2, F, points1, points2);
-
-  cv::Mat err(points1.size(), 1, CV_64FC1);
-  PM::_computeError(points1, points2, F_normalized, err);
-  int inlierCount = PM::_countBelow(err, 170);
-  std::cout << err << "\n";
-  std::cout << inlierCount << "\n";
-  */
-
-  //
-
-  int max_iter = 3000;
-  double threshold = 150;
+  // Find fundamental matrix with ransac
+  int max_iter = 1000;
+  double threshold = 350;
   double confidence = 99.0;
   PM::RANSACFundam ransac(max_iter, threshold, confidence);
 
-  cv::Mat F = PM::findEssential(points1, points2, k, ransac);
-  cv::Mat F_normalized = PM::_normalizeF(F);
-  drawEpipolarLines(img1, img2, F_normalized, points1, points2);
+  cv::Mat F = PM::findFundam(points1, points2, k, ransac);
+
+  // Visualize the epipolar lines
+  drawEpilines(img1, img2, F, points1, points2);
 
   return 0;
 }
--- include/fpsolver.h
+++ include/fpsolver.h
@@ -1,8 +1,6 @@
 #ifndef FPSOLVER_H
 #define FPSOLVER_H
 
-#include "Eigen/Dense"
-#include "ransac.h"
 #include <array>
 #include <iostream>
 #include <opencv2/core/mat.hpp>
@@ -58,7 +56,7 @@ public:
         cv::Mat err(points1.size(), 1, CV_64FC1);
         _computeError(points1, points2, F, err);
 
-        // todo: calculate inlierCount
+        // count the number of inliers
         int inlierCount = _countBelow(err, threshold);
         if (inlierCount > bestInlierCount) {
           besterr = err;
@@ -93,58 +91,9 @@ private:
   double threshold;
 };
 
-int _countBelow(const cv::Mat &err, double threshold) {
-  cv::Mat mask;
-  cv::threshold(err, mask, threshold, 255, cv::THRESH_BINARY_INV);
-  int count = cv::countNonZero(mask);
-  return count;
-}
-
-Eigen::Vector3d _backProjectPx(const Eigen::Vector3d &px,
-                               const Eigen::Matrix3d &intrinsic_matrix) {
+cv::Mat _backProjectPx(const cv::Vec3d &px, const cv::Mat &intrinsic_matrix) {
   // returning ray vector in camera coordinate from an image pixel
-  return intrinsic_matrix.inverse() * px;
-}
-
-std::vector<Eigen::Vector2d> _toEigen(const std::vector<cv::Point2f> &points) {
-  std::vector<Eigen::Vector2d> eigenPoints;
-  eigenPoints.reserve(points.size());
-
-  for (const auto &point : points) {
-    eigenPoints.emplace_back(point.x, point.y);
-  }
-
-  return eigenPoints;
-}
-
-Eigen::MatrixXd _toEigen(const cv::Mat &cvMat) {
-  // Check if the input cv::Mat is of type double
-  if (cvMat.type() != CV_64F) {
-    throw std::invalid_argument("cv::Mat must be of type CV_64F (double)");
-  }
-  Eigen::MatrixXd eigenMat(cvMat.rows, cvMat.cols);
-  for (int i = 0; i < cvMat.rows; ++i) {
-    for (int j = 0; j < cvMat.cols; ++j) {
-      eigenMat(i, j) = cvMat.at<double>(i, j);
-    }
-  }
-
-  return eigenMat;
-}
-
-std::vector<cv::Mat> _toCv(const std::vector<Eigen::MatrixXd> &eigenVector) {
-  std::vector<cv::Mat> cvMatVector;
-
-  for (const auto &eigenMat : eigenVector) {
-    cv::Mat cvMat(eigenMat.rows(), eigenMat.cols(), CV_64F);
-    for (int row = 0; row < eigenMat.rows(); ++row) {
-      for (int col = 0; col < eigenMat.cols(); ++col) {
-        cvMat.at<double>(row, col) = eigenMat(row, col);
-      }
-    }
-    cvMatVector.push_back(cvMat);
-  }
-  return cvMatVector;
+  return intrinsic_matrix.inv() * px;
 }
 
 cv::Mat _normalizeF(const cv::Mat &F) {
@@ -153,106 +102,112 @@ cv::Mat _normalizeF(const cv::Mat &F) {
   return F_normalized;
 }
 
-std::vector<Eigen::MatrixXd>
-findEssential(const std::vector<Eigen::Vector2d> &original_pixels,
-              const std::vector<Eigen::Vector2d> &corresponding_pixels,
-              const Eigen::Matrix3d &k) {
-  // checking for the right size of the input pixel correspondences
+std::vector<cv::Mat>
+findEssential(const std::vector<cv::Point2f> &original_pixels,
+              const std::vector<cv::Point2f> &corresponding_pixels,
+              const cv::Mat &k) {
+  // Check for the right size of the input pixel correspondences
   if (original_pixels.size() != corresponding_pixels.size()) {
     std::cout << "Same size pixels are required\n";
+    return {};
   } else if (original_pixels.size() < 2 || corresponding_pixels.size() < 2) {
     std::string pixel_size = std::to_string(original_pixels.size());
     std::cout << "Two point correspondences required, " << pixel_size
-              << " pixel is given";
+              << " pixels are given\n";
+    return {};
   }
 
-  std::vector<Eigen::Vector3d> op_cam;
-  std::vector<Eigen::Vector3d> cp_cam;
-  // normalize the point correspondences
+  std::vector<cv::Vec3d> op_cam;
+  std::vector<cv::Vec3d> cp_cam;
+
+  // Normalize the point correspondences
   for (int i = 0; i < 2; ++i) {
-    // convert to homogeneous points
-    Eigen::Vector3d op_h(original_pixels[i].x(), original_pixels[i].y(), 1.0);
-    Eigen::Vector3d cp_h(corresponding_pixels[i].x(),
-                         corresponding_pixels[i].y(), 1.0);
-    // backproject the homogenous points to camera points
+    // Convert to homogeneous points
+    cv::Vec3d op_h(original_pixels[i].x, original_pixels[i].y, 1.0);
+    cv::Vec3d cp_h(corresponding_pixels[i].x, corresponding_pixels[i].y, 1.0);
+
+    // Backproject the homogeneous points to camera points
     op_cam.push_back(_backProjectPx(op_h, k));
     cp_cam.push_back(_backProjectPx(cp_h, k));
   }
 
-  // the two feature points on the first image
-  double x1_l = op_cam[0].x();
-  double y1_l = op_cam[0].y();
-  double z1_l = op_cam[0].z();
-  double x2_l = op_cam[1].x();
-  double y2_l = op_cam[1].y();
-  double z2_l = op_cam[1].z();
+  // The two feature points on the first image
+  double x1_l = op_cam[0][0];
+  double y1_l = op_cam[0][1];
+  double z1_l = op_cam[0][2];
+  double x2_l = op_cam[1][0];
+  double y2_l = op_cam[1][1];
+  double z2_l = op_cam[1][2];
 
-  // the two feature points on the second image
-  double x1_r = cp_cam[0].x();
-  double y1_r = cp_cam[0].y();
-  double z1_r = cp_cam[0].z();
-  double x2_r = cp_cam[1].x();
-  double y2_r = cp_cam[1].y();
-  double z2_r = cp_cam[1].z();
+  // The two feature points on the second image
+  double x1_r = cp_cam[0][0];
+  double y1_r = cp_cam[0][1];
+  double z1_r = cp_cam[0][2];
+  double x2_r = cp_cam[1][0];
+  double y2_r = cp_cam[1][1];
+  double z2_r = cp_cam[1][2];
 
-  // constructing matrix A1 and A2 from the epipolar constraint
-  Eigen::Matrix2d A;
-  Eigen::Matrix2d B;
-  A << x1_l * y1_r, -z1_l * y1_r, x2_l * y2_r, -z2_l * y2_r;
-  B << x1_r * y1_l, z1_r * y1_l, x2_r * y2_l, z2_r * y2_l;
+  // Construct matrix A1 and A2 from the epipolar constraint
+  cv::Mat A = (cv::Mat_<double>(2, 2) << x1_l * y1_r, -z1_l * y1_r, x2_l * y2_r,
+               -z2_l * y2_r);
+  cv::Mat B = (cv::Mat_<double>(2, 2) << x1_r * y1_l, z1_r * y1_l, x2_r * y2_l,
+               z2_r * y2_l);
 
-  Eigen::MatrixXd C = B.inverse() * A;
-  Eigen::MatrixXd CTC = C.transpose() * C;
-  Eigen::JacobiSVD<Eigen::MatrixXd> svd(CTC, Eigen::ComputeThinU |
-                                                 Eigen::ComputeThinV);
-  Eigen::MatrixXd U = svd.matrixU();
-  Eigen::VectorXd S = svd.singularValues(); // Singular values (size 3)
-  double s1 = S(0);
-  double s2 = S(1);
+  cv::Mat C;
+  cv::invert(B, C, cv::DECOMP_SVD);
+  C = C * A;
+  cv::Mat CTC = C.t() * C;
 
-  std::vector<Eigen::MatrixXd> essentialMatrices;
-  // two possible solutions when s1 < 1
+  // Perform SVD
+  cv::SVD svd(CTC, cv::SVD::FULL_UV);
+  cv::Mat U = svd.u;
+  cv::Mat S = svd.w; // Singular values
+
+  double s1 = S.at<double>(0);
+  double s2 = S.at<double>(1);
+
+  std::vector<cv::Mat> essentialMatrices;
+
+  // Two possible solutions when s1 < 1
   if (s1 < 1) {
-    std::cout << " s1 < 1\n";
+    std::cout << "s1 < 1\n";
     for (int i = 0; i < 2; ++i) {
       double y1 = pow(-1, i);
       double y2 = 0;
-      Eigen::Vector2d y;
-      y << y1, y2;
-      Eigen::MatrixXd a = U * y;
-      Eigen::MatrixXd b = C * a;
-      Eigen::MatrixXd E(3, 3);
-      E << 0, b(0, 0), 0, -a(0, 0), 0, a(1, 0), 0, b(1, 0), 0;
+      cv::Mat y = (cv::Mat_<double>(2, 1) << y1, y2);
+      cv::Mat a = U * y;
+      cv::Mat b = C * a;
+      cv::Mat E = (cv::Mat_<double>(3, 3) << 0, b.at<double>(0), 0,
+                   -a.at<double>(0), 0, a.at<double>(1), 0, b.at<double>(1), 0);
       essentialMatrices.push_back(E);
     }
   }
-  // two possible solutions when s2 > 1
+  // Two possible solutions when s2 > 1
   else if (s2 > 1) {
-    std::cout << " s2 > 1\n";
+    std::cout << "s2 > 1\n";
     for (int i = 0; i < 2; ++i) {
       double y1 = 0;
       double y2 = pow(-1, i);
-      Eigen::Vector2d y;
-      y << y1, y2;
-      Eigen::MatrixXd a = U * y;
-      Eigen::MatrixXd b = C * a;
-      Eigen::MatrixXd E(3, 3);
-      E << 0, b(0, 0), 0, -a(0, 0), 0, a(1, 0), 0, b(1, 0), 0;
+      cv::Mat y = (cv::Mat_<double>(2, 1) << y1, y2);
+      cv::Mat a = U * y;
+      cv::Mat b = C * a;
+      cv::Mat E = (cv::Mat_<double>(3, 3) << 0, b.at<double>(0), 0,
+                   -a.at<double>(0), 0, a.at<double>(1), 0, b.at<double>(1), 0);
       essentialMatrices.push_back(E);
     }
   }
-  // four possible solutions when s1 >= 1 and s2 <=1
+  // Four possible solutions when s1 >= 1 and s2 <= 1
   else {
     for (int i = 0; i < 2; ++i) {
       for (int j = 0; j < 2; ++j) {
         double y1 = pow(-1, i) * sqrt((1 - s2) / (s1 - s2));
         double y2 = pow(-1, j) * sqrt((s1 - 1) / (s1 - s2));
-        Eigen::Vector2d y;
-        y << y1, y2;
-        Eigen::MatrixXd a = U * y;
-        Eigen::MatrixXd b = C * a;
-        Eigen::MatrixXd E(3, 3);
-        E << 0, b(0, 0), 0, -a(0, 0), 0, a(1, 0), 0, b(1, 0), 0;
+        cv::Mat y = (cv::Mat_<double>(2, 1) << y1, y2);
+        cv::Mat a = U * y;
+        cv::Mat b = C * a;
+        cv::Mat E =
+            (cv::Mat_<double>(3, 3) << 0, b.at<double>(0), 0, -a.at<double>(0),
+             0, a.at<double>(1), 0, b.at<double>(1), 0);
         essentialMatrices.push_back(E);
       }
     }
@@ -261,36 +216,13 @@ findEssential(const std::vector<Eigen::Vector2d> &original_pixels,
   return essentialMatrices;
 }
 
-std::vector<cv::Mat>
-findEssential(const std::vector<cv::Point2f> &original_pixels,
-              const std::vector<cv::Point2f> &corresponding_pixels,
-              const cv::Mat &k) {
-  std::vector<Eigen::Vector2d> orig_px_eig = _toEigen(original_pixels);
-  std::vector<Eigen::Vector2d> corr_px_eig = _toEigen(corresponding_pixels);
-  Eigen::MatrixXd k_eig = _toEigen(k);
-  std::vector<Eigen::MatrixXd> E_eig =
-      findEssential(orig_px_eig, corr_px_eig, k_eig);
-  std::vector<cv::Mat> E_Cv = _toCv(E_eig);
-  return E_Cv;
-}
-
-cv::Mat findEssential(const std::vector<cv::Point2f> &original_pixels,
-                      const std::vector<cv::Point2f> &corresponding_pixels,
-                      const cv::Mat &k, RANSACFundam ransac) {
+cv::Mat findFundam(const std::vector<cv::Point2f> &original_pixels,
+                   const std::vector<cv::Point2f> &corresponding_pixels,
+                   const cv::Mat &k, RANSACFundam ransac) {
 
   cv::Mat F = ransac.run(original_pixels, corresponding_pixels, k);
-  return F;
-}
-
-cv::Mat
-_estimateFundamental(const std::vector<cv::Point2f> &original_pixels,
-                     const std::vector<cv::Point2f> &corresponding_pixels,
-                     const cv::Mat &k) {
-  // this is for ransac
-  std::vector<cv::Mat> E =
-      findEssential(original_pixels, corresponding_pixels, k);
-  cv::Mat F = k.t().inv() * E[0] * k.inv();
-  return F;
+  cv::Mat F_normalized = _normalizeF(F);
+  return F_normalized;
 }
 
 void _computeError(std::vector<cv::Point2f> m1, std::vector<cv::Point2f> m2,
